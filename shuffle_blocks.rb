be54ffd0274e19d956f70d5f4e790e586962423e
sarah <sarahg.gray@gmail.com>
2013-01-06 00:13:26 -0600

    Remove ampersand when calling returned lambdas from namespaced module. Not sure why.

--- shuffle_blocks.rb
+++ shuffle_blocks.rb
@@ -28,13 +28,13 @@ module ColorMods
 end
 
 class ShuffledColors
-  def lighten_color(color, &color_modifier_block)
+  def lighten_color(color, color_modifier_block)
     color = (initial_color?) ? color : lighten(color)
     altered_color = color_modifier_block.call(color)
     self.shuffled_colors << [color, altered_color]
     return shuffled_colors if final_color?(altered_color)
 
-    lighten_color(altered_color, &color_modifier_block)
+    lighten_color(altered_color, color_modifier_block)
   end
 
   attr_accessor :shuffled_colors
@@ -54,8 +54,7 @@ class ShuffledColors
 end
 
 get '/' do
-  shuffler = ShuffledColors.new
-  colors = shuffler.lighten_color("blue", &make_bluer)
+  colors = ShuffledColors.new.lighten_color("blue", ColorMods.make_bluer)
 
   haml :index, :locals => {colors: colors}
 end
